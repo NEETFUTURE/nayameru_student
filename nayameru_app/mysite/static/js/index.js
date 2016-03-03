9cac1266f81aa21a8552b5a4833046eb5e82900b
readpeta <murakumo.room@gmail.com>
2016-03-03 20:37:32 +0900

    popover bug fixed

--- nayameru_app/mysite/static/js/index.js
+++ nayameru_app/mysite/static/js/index.js
@@ -1,14 +1,28 @@
 +$(function() {
 	'use strict';
-	$('#hoge').popover({
+
+	var pop_flg = false;
+	var hidePop = () =>{
+		$('#hoge').popover('hide');
+	};
+	$('#hoge').popover({	
 		trigger : 'click',
 		html: true,
-	});
-	$('#hoge').on('click', ()=>{
+		animation:true,
+	}).on('click', (e)=>{
 		$('#hoge + div').css('text-align', 'center');
 		$('.popover-content button').css('margin-bottom', '20px');
+		pop_flg = true;
 	});
-	$('.content').on('click', ()=>{
-		$('#hoge').popover('hide');
-	})
+
+	$('#login,#content').on('click', ()=>{
+		if(pop_flg){
+			$('#hoge').popover('hide').trigger('click');
+			pop_flg = false;
+		};
+	});
+	$(document).on('click', '#fuga', ()=> {
+		$('#hoge').popover('hide').trigger('click');
+		pop_flg = false;
+	});
 });
