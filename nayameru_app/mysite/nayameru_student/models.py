b3d7e6c7fdfc7daf243797bfe7e8f1ac2a7eb15b
NEETFUTURE <marumarumaru62@gmail.com>
2016-03-04 17:09:20 +0900

    SearchLogモデルを追加。外部キーとしてUserテーブルを仕様。

--- nayameru_app/mysite/nayameru_student/models.py
+++ nayameru_app/mysite/nayameru_student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class SearchLog(models.Model):
+    user_name = models.ForeignKey("User",
+                                on_delete=models.CASCADE)
+    book_name = models.CharField(max_length=200)
+    lowest_price = models.IntegerField()
+    at_data = models.DateTimeField('date published')
+    class Meta:
+        abstract = True
+
