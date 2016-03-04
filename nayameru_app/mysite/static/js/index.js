dc2d50cedc22a673473278144bfe8ce3fc1a1ee5
readpeta <murakumo.room@gmail.com>
2016-03-04 16:06:09 +0900

    もろもろの検索ボックスとか追加

--- nayameru_app/mysite/static/js/index.js
+++ nayameru_app/mysite/static/js/index.js
@@ -2,9 +2,6 @@
 	'use strict';
 
 	var pop_flg = false;
-	var hidePop = () =>{
-		$('#hoge').popover('hide');
-	};
 	$('#hoge').popover({	
 		trigger : 'click',
 		html: true,
@@ -12,17 +9,12 @@
 	}).on('click', (e)=>{
 		$('#hoge + div').css('text-align', 'center');
 		$('.popover-content button').css('margin-bottom', '20px');
-		pop_flg = true;
+	});a
+	$('#hoge').on('click', (e)=>{
+		e.stopPropagation();
+	});
+	$('html').on('click', ()=>{
+		$('#hoge').popover('hide');
 	});
 
-	$('#login,#content').on('click', ()=>{
-		if(pop_flg){
-			$('#hoge').popover('hide').trigger('click');
-			pop_flg = false;
-		};
-	});
-	$(document).on('click', '#fuga', ()=> {
-		$('#hoge').popover('hide').trigger('click');
-		pop_flg = false;
-	});
 });
