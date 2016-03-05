af0057e27ae5fca647b168913f4a9332f2011d48
NEETFUTURE <marumarumaru62@gmail.com>
2016-03-05 20:03:03 +0900

    viewに検索のためのurlを追加 ただし中身はまだ空っぽ

--- nayameru_app/mysite/nayameru_student/urls.py
+++ nayameru_app/mysite/nayameru_student/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     url(r"^$", views.test, name="test"),
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='find_book'),
 ]
--- nayameru_app/mysite/nayameru_student/views.py
+++ nayameru_app/mysite/nayameru_student/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.template import Context, loader
+import amazonAPI
 
 # Create your views here.
 def test(request):
@@ -6,3 +8,8 @@ def test(request):
 
 def index(request):
     return HttpResponse("This is an index page.")
+
+def find_book(request,q):
+    books_data={}
+    return render_to_response("index.html",
+                              {"books_data":books_data})
