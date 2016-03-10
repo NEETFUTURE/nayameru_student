5534ab505a6b1105ca3a5e7448bddb5a54c4d81b
NEETFUTURE <marumarumaru62@gmail.com>
2016-03-10 16:33:39 +0900

    本格的に検索機能のためのコードを追加

--- nayameru_app/mysite/nayameru_student/views.py
+++ nayameru_app/mysite/nayameru_student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, render_to_response
 from django.template import Context, loader
 import amazonAPI
 
@@ -10,6 +10,9 @@ def index(request):
     return HttpResponse("This is an index page.")
 
 def find_book(request,q):
-    books_data={}
+    myapi = amazonAPI("AK", "SK", "AT")
+    myapi.search(q["word"])
+    books_data = [myapi.get_with_number(number=i) for i in range(1,11)]
+
     return render_to_response("index.html",
                               {"books_data":books_data})
