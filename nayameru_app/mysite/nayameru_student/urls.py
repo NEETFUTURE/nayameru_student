e9243241a933a40e50d4329260a516138bdfba64
NEETFUTURE <marumarumaru62@gmail.com>
2016-03-11 02:51:32 +0900

    トップページで検索窓から検索できるようにした 結果はとりあえずコンソールに表示 しかしボタンが反応しないので検索できず

--- nayameru_app/mysite/nayameru_student/urls.py
+++ nayameru_app/mysite/nayameru_student/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
+    url(r'^find_book$', views.find_book, name='find_book'),
     url(r"^$", views.test, name="test"),
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.index, name='find_book'),
 ]
--- nayameru_app/mysite/nayameru_student/views.py
+++ nayameru_app/mysite/nayameru_student/views.py
@@ -9,10 +9,12 @@ def test(request):
 def index(request):
     return HttpResponse("This is an index page.")
 
-def find_book(request,q):
+def find_book(request):
     myapi = amazonAPI("AK", "SK", "AT")
-    myapi.search(q["word"])
+    w = request.POST['search_word']
+    myapi.search(w)
     books_data = [myapi.get_with_number(number=i) for i in range(1,11)]
+    print(books_data)
 
     return render_to_response("index.html",
                               {"books_data":books_data})
