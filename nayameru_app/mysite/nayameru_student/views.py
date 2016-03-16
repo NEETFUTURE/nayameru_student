39af4497d1268ac9e671c941df63cae7ddd45e35
NEETFUTURE <marumarumaru62@gmail.com>
2016-03-16 21:38:37 +0900

    indexのテンプレートを追加修正 これで検索結果が5x2で表示されるはず

--- nayameru_app/mysite/nayameru_student/views.py
+++ nayameru_app/mysite/nayameru_student/views.py
@@ -16,8 +16,9 @@ def find_book(request):
     myapi = amazonAPI("AK", "SK", "AT")
     w = request.POST['search_word']
     myapi.search(w)
-    books_data = [myapi.get_with_number(number=i) for i in range(1,11)]
+    books_data = [(myapi.get_with_number(number=(2*i-2)),
+                   myapi.get_with_number(number=(2*i-1))) for i in range(1,6)]
     print(books_data)
-    payload.update(books_data)
+    payload.update({"books_data":books_data})
     return render_to_response("index.html",
                               payload)
