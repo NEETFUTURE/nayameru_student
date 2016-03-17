3b53b77518855f44bae613d78d71bebc8dfa673a
tableamd <espoir204@softbank.ne.jp>
2016-03-17 13:09:56 +0900

    SearchLog周りとnayameru_studentのviewの一部を変更

--- nayameru_app/mysite/nayameru_student/models.py
+++ nayameru_app/mysite/nayameru_student/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from userauth.models import CustomUser
 
 # Create your models here.
 
 class SearchLog(models.Model):
-    user_name = models.ForeignKey("User",
+    user = models.ForeignKey(CustomUser,
                                 on_delete=models.CASCADE)
     book_name = models.CharField(max_length=200)
     lowest_price = models.IntegerField()
     at_data = models.DateTimeField('date published')
+
     class Meta:
         abstract = True
 
--- nayameru_app/mysite/nayameru_student/views.py
+++ nayameru_app/mysite/nayameru_student/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, HttpResponse, render_to_response
 from django.template import Context, loader
 from django.core.context_processors import csrf
-import amazonAPI
+#import amazonAPI
+from .amazonAPI import amazonAPI
+from django.contrib.auth.decorators import login_required
+from .models import SearchLog
 
-# Create your views here.
+
+#@login_required #ログイン必須にするデコレータ
 def test(request):
     return render(request, "index.html", {})
+
 
 def index(request):
     return HttpResponse("This is an index page.")
 
+
+#@login_required #ログイン必須にするデコレータ
 def find_book(request):
+    #教科書名1,教科書名2,教科書名3,... みたいな感じの複数教科書名検索に対応させる必要あり…？
+
     payload = {}
     payload.update(csrf(request))
     myapi = amazonAPI("AK", "SK", "AT")
@@ -20,5 +29,24 @@ def find_book(request):
                    myapi.get_with_number(number=(2*i-1))) for i in range(1,6)]
     print(books_data)
     payload.update({"books_data":books_data})
+
+    #検索ログの保存
+    # new_log = SearchLog(
+    #     user_id=request.user.id,
+    #     book_name=w,
+    #     lowest_price=0
+    # )
+    # new_log.save()
+
     return render_to_response("index.html",
                               payload)
+
+
+#viewのコントローラ内部でのみ利用可能
+#自分と同じ大学のユーザの検索履歴を取ってくる
+# def osusume(req):
+#     universityname = req.user.universityname
+#     logs = SearchLog.objects.filter(user__universityname=universityname)
+#     tmp = [log.book_name for log in logs]
+#
+#     return tmp
